0a8fe495cd744846bc842697858730c90e44de5c
pawanjay176 <pawandhananjay@gmail.com>
2019-05-12 22:40:39 +0530

    Refactor for object oriented approach * Create SecretSharer trait. * Have default implementation for generate_shares and reconstruct_secret. * Have PlaintextSharer for Plaintext secrets implementing trait.

--- examples/ss_example.rs
+++ examples/ss_example.rs
@@ -1,15 +1,14 @@
 extern crate secretsharing;
 
-use secretsharing::ss::{Charset, SecretSharing};
+use secretsharing::ss::{PlaintextSecretSharer, SecretSharer};
 
 fn main() {
     let threshold = 3;
     let total_shares = 5;
     let secret = "HelloWorld123";
-    let mut ss = SecretSharing::new(
+    let mut ss = PlaintextSecretSharer::new(
         threshold,             // Minimum shares required for reconstruction.
         total_shares,          // Total shares.
-        Charset::Alphanumeric, // Charset of secret.
     );
     let shares = ss.generate_shares(secret).unwrap();
     let reconstructed_secret = ss.reconstruct_secret(&shares[0..3].to_vec()).unwrap();
--- src/ss.rs
+++ src/ss.rs
@@ -2,60 +2,14 @@ use crate::utils;
 use crate::utils::SSError;
 use num::BigInt;
 
-/// Secret sharing struct.
-pub struct SecretSharing {
-    /// Minimum number of shares required to recover the secret.
-    threshold: u32,
-    /// Total number of parties the secret is split amongst.
-    total: u32,
-    /// The character set used by the secret.
-    charset: Charset,
-    /// Prime number used for the prime field for polynomial operations.
-    prime: Option<BigInt>,
-}
-
-impl SecretSharing {
-    pub fn new(threshold: u32, total: u32, charset: Charset) -> Self {
-        SecretSharing {
-            threshold,
-            total,
-            charset,
-            prime: None,
-        }
-    }
-
-    /// Minimum number of shares required to reconstruct secret.
-    #[inline]
-    pub fn threshold(&self) -> u32 {
-        self.threshold
-    }
-
-    /// Total number of parties the secret is split amongst.
-    #[inline]
-    pub fn total(&self) -> u32 {
-        self.total
-    }
-
-    /// Charset in which the secret is represented.
-    #[inline]
-    pub fn charset(&self) -> &str {
-        self.charset.charset_str()
-    }
-
-    /// Set prime in which field operations are performed.
-    #[inline]
-    fn set_prime(&mut self, prime: BigInt) {
-        self.prime = Some(prime)
-    }
-
-    #[inline]
-    /// Prime number on which the field operations are performed.
-    pub fn prime(&self) -> Result<&BigInt, SSError> {
-        self.prime.as_ref().ok_or(SSError::PrimeNotSet)
-    }
-
+pub trait SecretSharer {
+    fn threshold(&self) -> u32;
+    fn total(&self) -> u32;
+    fn charset(&self) -> &str;
+    fn set_prime(&mut self, prime: BigInt);
+    fn prime(&self) -> Result<&BigInt, SSError>;
     /// Split a secret to shares based on SecretSharing params.
-    pub fn generate_shares(&mut self, secret: &str) -> Result<Vec<String>, SSError> {
+    fn generate_shares(&mut self, secret: &str) -> Result<Vec<String>, SSError> {
         // Need threshold to be atleast 2.
         if self.threshold() < 2 {
             return Err(SSError::LowThreshold);
@@ -80,7 +34,7 @@ impl SecretSharing {
     }
 
     /// Reconstruct secret from shares.
-    pub fn reconstruct_secret(&self, shares: &[String]) -> Result<String, SSError> {
+    fn reconstruct_secret(&self, shares: &[String]) -> Result<String, SSError> {
         // Not enough shares to reconstruct secret.
         if (shares.len() as u32) < self.threshold() {
             return Err(SSError::InsufficientShares);
@@ -94,6 +48,58 @@ impl SecretSharing {
         let secret_int = utils::points_to_secret_int(point_shares?, self.prime()?)?;
         // Convert secret_int to secret based on charset.
         utils::int_to_charset_repr(secret_int, self.charset())
+    }
+}
+
+
+/// Secret sharing struct.
+pub struct PlaintextSecretSharer {
+    /// Minimum number of shares required to recover the secret.
+    threshold: u32,
+    /// Total number of parties the secret is split amongst.
+    total: u32,
+    /// Prime number used for the prime field for polynomial operations.
+    prime: Option<BigInt>,
+}
+
+impl PlaintextSecretSharer {
+    pub fn new(threshold: u32, total: u32) -> Self {
+        PlaintextSecretSharer {
+            threshold,
+            total,
+            prime: None,
+        }
+    }
+}
+impl SecretSharer for PlaintextSecretSharer {
+    /// Minimum number of shares required to reconstruct secret.
+    #[inline]
+    fn threshold(&self) -> u32 {
+        self.threshold
+    }
+
+    /// Total number of parties the secret is split amongst.
+    #[inline]
+    fn total(&self) -> u32 {
+        self.total
+    }
+
+    /// Charset in which the secret is represented.
+    #[inline]
+    fn charset(&self) -> &str {
+        Charset::Alphanumeric.charset_str()
+    }
+
+    /// Set prime in which field operations are performed.
+    #[inline]
+    fn set_prime(&mut self, prime: BigInt) {
+        self.prime = Some(prime)
+    }
+
+    #[inline]
+    /// Prime number on which the field operations are performed.
+    fn prime(&self) -> Result<&BigInt, SSError> {
+        self.prime.as_ref().ok_or(SSError::PrimeNotSet)
     }
 }
 
